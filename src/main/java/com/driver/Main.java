f3bfd3c9f1df2c1403f7b4797175f18d80805de3
akashsInterra <109330154+akashsInterra@users.noreply.github.com>
2023-01-13 17:56:16 +0530

    first sub

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly obj=new RWOnly();
+      obj.setName("raj");
+      System.out.print (obj.getName() );
+  }
 }
